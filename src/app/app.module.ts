a92098a7164e08be54f6034a783beeaa2f648377
CristianTafur249 <tafurcristian196@gmail.com>
2022-10-21 23:01:24 -0500

    Icono y arreglo de bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ImgComponent } from './componentes/img/img.component';
-import { ProductoComponent } from './componentd/producto/producto.component';
-import { ProducComponent } from './components/produc/produc.component';
+import { ImgComponent } from './components/img/img.component';
 import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ImgComponent,
-    ProductoComponent,
-    ProducComponent,
     ProductComponent
   ],
   imports: [
